c91bf9f9bb401b83eea9231761a8a487c6712090
Giang Pham <giang.pham@zalando.fi>
2018-09-27 09:46:28 +0300

    Define user cache data store

---
+++ app/src/main/java/leo/me/la/codeblue/cache/UserCacheDataStore.kt
@@ -0,0 +1,36 @@
+package leo.me.la.codeblue.cache
+
+import io.reactivex.Completable
+import io.reactivex.Single
+import leo.me.la.codeblue.remote.User
+
+interface UserCacheDataStore {
+    fun storeUserInfo(user: User): Completable
+    fun loadUserInfo(userId: Int): Single<CacheUser>
+}
+
+class UserCacheDataStoreImpl(
+    private val userDao: UserDao
+) : UserCacheDataStore {
+    override fun storeUserInfo(user: User): Completable {
+        return Completable.fromAction {
+            userDao.insert(
+                CacheUser(
+                    userId = user.userId,
+                    firstName = user.firstName,
+                    lastName = user.lastName,
+                    birthdate = user.birthdate,
+                    gender = user.gender,
+                    weight = user.weight,
+                    height = user.height
+                )
+            )
+        }
+    }
+
+    override fun loadUserInfo(userId: Int): Single<CacheUser> {
+        return userDao.getUser(userId)
+    }
+}
+
+
