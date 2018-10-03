c2d6da654640d08e88b3be7cc2cc6b9d480c7102
Dat Truong <dat.truonghuu1212@gmail.com>
2018-10-03 19:02:54 +0300

    Remove unnecessary function and improve code

--- app/src/main/java/leo/me/la/codeblue/AugmentedImageActivity.kt
+++ app/src/main/java/leo/me/la/codeblue/AugmentedImageActivity.kt
@@ -2,8 +2,6 @@ package leo.me.la.codeblue
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.ImageView
 import com.google.ar.core.AugmentedImage
 import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.FrameTime
@@ -12,38 +10,33 @@ import leo.me.la.codeblue.common.helper.SnackbarHelper
 
 class AugmentedImageActivity : AppCompatActivity() {
 
-    private var arFragment: ArFragment? = null
-    private var fitToScanView: ImageView? = null
+    private val arFragment: ArFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_augmented_image)
-
-        arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment?
-        fitToScanView = findViewById(R.id.image_view_fit_to_scan)
-
-        arFragment!!.arSceneView.scene.addOnUpdateListener(onUpdateFrame)
     }
-    /**
-     * Registered with the Sceneform Scene object, this method is called at the start of each frame.
-     *
-     * @param frameTime - time since last frame.
-     */
+
+    override fun onResume() {
+        super.onResume()
+        arFragment.arSceneView.scene.addOnUpdateListener(onUpdateFrame)
+    }
 
     private val onUpdateFrame: (FrameTime) -> Unit = {
-        val frame = arFragment!!.arSceneView.arFrame
+        val frame = arFragment.arSceneView.arFrame
 
         // If there is no frame or ARCore is not tracking yet, just return.
         if (!(frame == null || frame.camera.trackingState !== TrackingState.TRACKING)) {
-            val updatedAugmentedImages = frame!!.getUpdatedTrackables(AugmentedImage::class.java)
-            for (augmentedImage in updatedAugmentedImages) {
-                when (augmentedImage.trackingState) {
+            val updatedAugmentedImages = frame.getUpdatedTrackables(AugmentedImage::class.java)
+            updatedAugmentedImages.forEach {
+                when (it.trackingState) {
                     TrackingState.PAUSED -> {
                         // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
                         // but not yet tracked.
-                        val text = "Detected Image " + augmentedImage.index
+                        val text = "Detected Image " + it.index
                         SnackbarHelper.instance.showMessage(this, text)
-
                         removeListener()
                     }
                     else -> { }
@@ -53,6 +46,6 @@ class AugmentedImageActivity : AppCompatActivity() {
     }
 
     private fun removeListener() {
-        arFragment!!.arSceneView.scene.removeOnUpdateListener(onUpdateFrame)
+        arFragment.arSceneView.scene.removeOnUpdateListener(onUpdateFrame)
     }
 }
