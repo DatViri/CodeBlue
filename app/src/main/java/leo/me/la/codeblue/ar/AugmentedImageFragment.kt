fe3fc4c22778487a5a35c56adb61338a6ca8ca1c
Giang Pham <giang.pham@zalando.fi>
2018-10-06 15:46:44 +0300

    Simplify augmented image fragment

--- app/src/main/java/leo/me/la/codeblue/ar/AugmentedImageFragment.kt
+++ app/src/main/java/leo/me/la/codeblue/ar/AugmentedImageFragment.kt
@@ -6,7 +6,6 @@ import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,17 +22,16 @@ class AugmentedImageFragment : ArFragment() {
         super.onAttach(context)
 
         val openGlVersionString = (context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
-                .deviceConfigurationInfo
-                .glEsVersion
+            .deviceConfigurationInfo
+            .glEsVersion
         if (java.lang.Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 or later")
             SnackbarHelper.instance
-                    .showError(activity!!, "Sceneform requires OpenGL ES 3.0 or later")
+                .showError(activity!!, "Sceneform requires OpenGL ES 3.0 or later")
         }
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = super.onCreateView(inflater, container, savedInstanceState)
 
         // Turn off the plane discovery since we're only looking for images
@@ -47,7 +45,7 @@ class AugmentedImageFragment : ArFragment() {
         val config = super.getSessionConfiguration(session)
         if (!setupAugmentedImageDatabase(config, session)) {
             SnackbarHelper.instance
-                    .showError(activity!!, "Could not setup augmented image database")
+                .showError(activity!!, "Could not setup augmented image database")
         }
         return config
     }
@@ -55,38 +53,12 @@ class AugmentedImageFragment : ArFragment() {
     private fun setupAugmentedImageDatabase(config: Config, session: Session): Boolean {
         var augmentedImageDatabase: AugmentedImageDatabase? = null
 
-        val assetManager = if (context != null) context!!.assets else null
-        if (assetManager == null) {
-            Log.e(TAG, "Context is null, cannot intitialize image database.")
-            return false
-        }
+        val assetManager = (if (context != null) context!!.assets else null) ?: return false
 
-        // There are two ways to configure an AugmentedImageDatabase:
-        // 1. Add Bitmap to DB directly
-        // 2. Load a pre-built AugmentedImageDatabase
-        // Option 2) has
-        // * shorter setup time
-        // * doesn't require images to be packaged in apk.
-        if (USE_SINGLE_IMAGE) {
-            val augmentedImageBitmap = loadAugmentedImageBitmap(assetManager) ?: return false
+        val augmentedImageBitmap = loadAugmentedImageBitmap(assetManager) ?: return false
 
-            augmentedImageDatabase = AugmentedImageDatabase(session)
-            augmentedImageDatabase.addImage(DEFAULT_IMAGE_NAME, augmentedImageBitmap)
-            // If the physical size of the image is known, you can instead use:
-            //     augmentedImageDatabase.addImage("image_name", augmentedImageBitmap, widthInMeters);
-            // This will improve the initial detection speed. ARCore will still actively estimate the
-            // physical size of the image as it is viewed from multiple viewpoints.
-        } else {
-            // This is an alternative way to initialize an AugmentedImageDatabase instance,
-            // load a pre-existing augmented image database.
-            try {
-                context!!.assets.open(SAMPLE_IMAGE_DATABASE).use { `is` -> augmentedImageDatabase = AugmentedImageDatabase.deserialize(session, `is`) }
-            } catch (e: IOException) {
-                Log.e(TAG, "IO exception loading augmented image database.", e)
-                return false
-            }
-
-        }
+        augmentedImageDatabase = AugmentedImageDatabase(session)
+        augmentedImageDatabase.addImage(DEFAULT_IMAGE_NAME, augmentedImageBitmap)
 
         config.augmentedImageDatabase = augmentedImageDatabase
         return true
@@ -95,27 +67,18 @@ class AugmentedImageFragment : ArFragment() {
     private fun loadAugmentedImageBitmap(assetManager: AssetManager?): Bitmap? {
         try {
             assetManager!!.open(DEFAULT_IMAGE_NAME).use { `is` -> return BitmapFactory.decodeStream(`is`) }
-        } catch (e: IOException) {
-            Log.e(TAG, "IO exception loading augmented image bitmap.", e)
+        } catch (ignore: IOException) {
         }
 
         return null
     }
 
     companion object {
-        private var TAG = "AugmentedImageFragment"
 
         // This is the name of the image in the sample database.  A copy of the image is in the assets
         // directory.  Opening this image on your computer is a good quick way to test the augmented image
         // matching.
         private var DEFAULT_IMAGE_NAME = "default.jpg"
-
-        // This is a pre-created database containing the sample image.
-        private var SAMPLE_IMAGE_DATABASE = "sample_database.imgdb"
-
-        // Augmented image configuration and rendering.
-        // Load a single image (true) or a pre-generated image database (false).
-        private var USE_SINGLE_IMAGE = false
 
         // Do a runtime check for the OpenGL level available at runtime to avoid Sceneform crashing the
         // application.
