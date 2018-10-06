b3f8bc5bab58e50ebbc694f70491afdea79267e5
Giang Pham <giang.pham@zalando.fi>
2018-10-06 19:00:30 +0300

    Update token

--- app/src/main/java/leo/me/la/codeblue/remote/PolarApi.kt
+++ app/src/main/java/leo/me/la/codeblue/remote/PolarApi.kt
@@ -10,7 +10,7 @@ interface PolarApi {
     @GET("v3/users/{user-id}")
     fun getUserInfo(
         @Path("user-id") userId: Int,
-        @Header("Authorization") token: String = "Bearer caf5efd8628fe216c30b08cd63649edc"
+        @Header("Authorization") token: String = "Bearer 0b28742a663b3cf328217021819b7af9"
     ) : Single<User>
 }
 
