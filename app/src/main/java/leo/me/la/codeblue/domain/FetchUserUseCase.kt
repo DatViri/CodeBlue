e83564a62ca425dad7c3fd5a5dbbb117b842f8bb
Giang Pham <giang.pham@zalando.fi>
2018-10-06 15:45:23 +0300

    Map user data from birthdate to age

--- app/src/main/java/leo/me/la/codeblue/domain/FetchUserUseCase.kt
+++ app/src/main/java/leo/me/la/codeblue/domain/FetchUserUseCase.kt
@@ -3,16 +3,42 @@ package leo.me.la.codeblue.domain
 import io.reactivex.Scheduler
 import io.reactivex.Single
 import leo.me.la.codeblue.data.UserRepository
-import leo.me.la.codeblue.remote.User
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class FetchUserUseCase(
     private val userRepository: UserRepository,
     private val threadExecutor: Scheduler,
     private val postExecutionThread: Scheduler
 ) {
-    fun fetchUserUseCase(userId: Int) : Single<User> {
+    fun fetchUserUseCase(userId: Int) : Single<DataUser> {
         return userRepository.getUser(userId)
+            .map {
+                val year = Calendar.getInstance()
+                    .also { cal ->
+                        cal.time = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(it.birthdate)
+                    }
+                    .get(Calendar.YEAR)
+                DataUser(
+                    it.firstName,
+                    it.lastName,
+                    Calendar.getInstance().get(Calendar.YEAR) - year,
+                    it.gender,
+                    it.weight,
+                    it.height
+                )
+            }
             .subscribeOn(threadExecutor)
             .observeOn(postExecutionThread)
     }
 }
+
+data class DataUser(
+    val firstName: String,
+    val lastName: String,
+    val age: Int,
+    val gender: String,
+    val weight: Int,
+    val height: Int
+)
